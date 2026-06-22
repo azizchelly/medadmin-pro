9218c36ffbc5e24a37919912bfd138bf80e4b51e
azizchelly <132477652+azizchelly@users.noreply.github.com>
2026-06-22 12:21:17 +0100

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-
-
-const SUPABASE_URL = 'https://esdzbjnlkyqvnuzyyooj.supabase.co';       // ex: https://xxxx.supabase.co
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzZHpiam5sa3lxdm51enl5b29qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxMTYwMjEsImV4cCI6MjA5NzY5MjAyMX0.AIYNqfrddwS5kPcROwQBNAQmQ0EBZ7lChyYQGLuq1ZE';      // clé "anon public"
-
-// Initialisation du client Supabase
-const { createClient } = supabase;
-const db = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
